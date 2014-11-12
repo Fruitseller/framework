306d7886c41ae232941b5d46c79d2ba271d47ad3
Peter Bronkalla <peterbronkalla@googlemail.com>
2014-11-12 20:12:58 +0100

    Added Whoops and initialized it in Application

--- Core/Application.php
+++ Core/Application.php
@@ -2,8 +2,19 @@
 
 namespace framework\Core;
 
+use Whoops\Run as WhoopsRun;
+use Whoops\Handler\PrettyPageHandler as WhoopsPrettyPageHandler;
+
 class Application {
 	public function run() {
-		echo "App running";
+		$this->initWhoops();
+	}
+
+	private function initWhoops() {
+		$whoops = new WhoopsRun();
+		$handler = new WhoopsPrettyPageHandler();
+		$whoops->pushHandler($handler)->register();
+
+		return $this;
 	}
 }
