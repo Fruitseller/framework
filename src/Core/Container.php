b30c4875104df4ca817a98b6afd625ae7732e54e
Peter Bronkalla <peterbronkalla@googlemail.com>
2015-01-12 21:54:54 +0100

    Added support for singletons in Container class

--- src/Core/Container.php
+++ src/Core/Container.php
@@ -8,10 +8,15 @@ use ArrayAccess;
 class Container implements ArrayAccess {
 
 	protected $bindings = [];
+	protected $instances = [];
 
-	public function bind($key, $value) {
-		
-		$this->bindings[$key] = $value;
+	public function bind($key, $value, $singleton = false) {
+
+		$this->bindings[$key] = compact('value', 'singleton');
+	}
+
+	public function singleton($key, $value) {
+		return $this->bind($key, $value, true);
 	}
 
 	public function getBinding($key) {
@@ -23,18 +28,52 @@ class Container implements ArrayAccess {
 		return $this->bindings[$key];
 	}
 
+	public function isSingleton($key) {
+		$binding = $this->getBinding($key);
+
+		if ($binding === null) {
+			return false;
+		}
+
+		return $binding['singleton'];
+	}
+
+	public function singletonResolved($key) {
+		return array_key_exists($key, $this->instances);
+	}
+
+	public function getSingletonInstance($key) {
+		return $this->singletonResolved($key) ? $this->instances[$key] : null;
+	}
+
 	public function resolve($key, array $args = array()) {
-		
+
 		$class = $this->getBinding($key);
 
 		if ($class === null) {
 			$class = $key;
 		}
 
-		return $this->buildObject($class, $args);
+		if ($this->isSingleton($key) && $this->singletonResolved($key)) {
+			return $this->getSingletonInstance($key);
+		}
+
+		$object = $this->buildObject($class, $args);
+
+		return $this->prepareObject($key, $object);
 	}
 
-	protected function buildObject($className, array $args = array()) {
+	protected function prepareObject($key, $object) {
+		if ($this->isSingleton($key)) {
+			$this->instances[$key] = $object;
+		}
+
+		return $object;
+	}
+
+	protected function buildObject($class, array $args = array()) {
+
+		$className = $class['value'];
 
 		$reflector = new ReflectionClass($className);
 
@@ -43,31 +82,37 @@ class Container implements ArrayAccess {
 		}
 
 		if ($reflector->getConstructor() !== null) {
-			
+
 			$constructor = $reflector->getConstructor();
 			$dependencies = $constructor->getParameters();
 
-			foreach ($dependencies as $dependency) {
-				
-				if ($dependency->isOptional()) continue;
-				if ($dependency->isArray()) continue;
-
-				$class = $dependency->getClass();
-
-				if ($class === null) continue;
-
-				if (get_class($this) === $class->name) {
-					array_unshift($args, $this);
-					continue;
-				}
-
-				array_unshift($args, $this->resolve($class->name));
-			}
+			$args = $this->buildDependencies($args, $dependencies, $class);
 		}
 
 		$object = $reflector->newInstanceArgs($args);
 
 		return $object;
+	}
+
+	protected function buildDependencies($args, $dependencies, $class) {
+		foreach ($dependencies as $dependency) {
+
+			if ($dependency->isOptional()) continue;
+			if ($dependency->isArray()) continue;
+
+			$class = $dependency->getClass();
+
+			if ($class === null) continue;
+
+			if (get_class($this) === $class->name) {
+				array_unshift($args, $this);
+				continue;
+			}
+
+			array_unshift($args, $this->resolve($class->name));
+		}
+
+		return $args;
 	}
 
 	public function offsetGet($key) {
